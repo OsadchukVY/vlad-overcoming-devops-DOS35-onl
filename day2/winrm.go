924329bfefe537464348df31a26e9a44f11d7431
Linus Tovralds <bill.gates@microsoft.com>
2026-04-05 17:50:44 +0300

    Add infinite user input

--- day2/winrm.go
+++ day2/winrm.go
@@ -9,10 +9,12 @@ import (
 
 func main() {
 	inputReader := bufio.NewReader(os.Stdin)
-	userInput, err := inputReader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	for {
+		userInput, err := inputReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(userInput)
 	}
 
-	fmt.Println(userInput)
 }
