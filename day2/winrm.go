d6a754bb6ced17107fcedfaa78fe1053dfe9ef82
Linus Torvalds <bill.gates@microsoft.com>
2026-04-05 19:54:55 +0300

    Fix go project init

--- day2/winrm.go
+++ day2/winrm.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"github.com/masterzen/winrm"
 	"log"
 	"os"
 )
 
+func winrm_connect(server, user, password, command string) {
+	endpoint := winrm.NewEndpoint(server, 5986, false, false, nil, nil, nil, 0)
+	client, err := winrm.NewClient(endpoint, user, password)
+	if err != nil {
+		panic(err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	client.RunWithContext(ctx, command, os.Stdout, os.Stderr)
+}
+
 func userInput() (string, error) {
+	fmt.Println("shell :>")
 	inputReader := bufio.NewReader(os.Stdin)
 	userInput, err := inputReader.ReadString('\n')
 	if err != nil {
