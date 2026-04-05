bc77d99730961dd917996b851ee3895e594b9eaf
Linus Torvalds <bill.gates@microsoft.com>
2026-04-05 20:30:02 +0300

    Add flags

--- day2/winrm.go
+++ day2/winrm.go
@@ -3,18 +3,33 @@ package main
 import (
 	"bufio"
 	"context"
+	"flag"
 	"fmt"
 	"github.com/masterzen/winrm"
 	"log"
 	"os"
 )
 
-func winrm_connect(server, user, password, command string) {
-	endpoint := winrm.NewEndpoint(server, 5986, false, false, nil, nil, nil, 0)
+var (
+	server   string
+	user     string
+	password string
+	port     int
+)
+
+func init() {
+	flag.StringVar(&server, "server", "127.0.0.1", "set  destination server IP")
+	flag.StringVar(&user, "user", "", "Set username")
+	flag.IntVar(&port, "port", 5985, "Set port")
+}
+
+func winrm_connect(server, user, password, command string, port int) {
+	endpoint := winrm.NewEndpoint(server, port, false, false, nil, nil, nil, 0)
 	client, err := winrm.NewClient(endpoint, user, password)
 	if err != nil {
 		panic(err)
 	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	client.RunWithContext(ctx, command, os.Stdout, os.Stderr)
@@ -29,14 +44,33 @@ func userInput() (string, error) {
 	}
 	return userInput, nil
 }
+
+func enterPassword() (string, error) {
+	fmt.Println("Enter password :>")
+	inputReader := bufio.NewReader(os.Stdin)
+	userInput, err := inputReader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return userInput, nil
+}
+
 func main() {
+	var password, err = enterPassword()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for {
+
 		command, err := userInput()
+
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		winrm_connect(server, user, password, command, port)
 		fmt.Println(command)
+
 	}
 }
